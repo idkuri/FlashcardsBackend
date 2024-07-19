6fcb4258dffdfd1074e34a1edccba0b03b72e4f6
unknown <acaoy2@gmail.com>
2024-07-19 06:57:06 -0400

    new end point added

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import get_json_data
+from .views import get_json_data, get_correct_image_view, get_incorrect_image_view
 
 urlpatterns = [
     path('data/', get_json_data, name='get_json_data'),
+    path('correct/', get_correct_image_view, name='get_correct_image_view'),
+    path('incorrect/', get_incorrect_image_view, name='get_incorrect_image_view')
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,27 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.http import FileResponse, Http404
 from .utils import read_json_file
+import os
 
 @api_view(['GET'])
 def get_json_data(request):
     data = read_json_file('chinese.json')
     return Response(data)
+
+
+@api_view(['GET'])
+def get_correct_image_view(request):
+    try:
+        file_path = os.path.join(os.path.dirname(__file__), "correct.png")
+        return FileResponse(open(file_path, 'rb'), content_type='image/png')
+    except FileNotFoundError:
+        raise Http404("Image not found")
+
+@api_view(['GET'])
+def get_incorrect_image_view(request):
+    try:
+        file_path = os.path.join(os.path.dirname(__file__), "incorrect.png")
+        return FileResponse(open(file_path, 'rb'), content_type='image/png')
+    except FileNotFoundError:
+        raise Http404("Image not found")
